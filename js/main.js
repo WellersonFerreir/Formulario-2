605659e59331f1d9763b13e4455b7fd17ed695f3
Wellerson Ferreira <117539408+WellersonFerreir@users.noreply.github.com>
2023-01-15 18:47:12 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,28 @@
 document.getElementById("continuar").addEventListener("click", function () {
-    let name = document.getElementById("input").value;
+    let name = document.getElementById("name").value;
     if (!name) {
-        alert("Todos os campos devem ser preenchidos.");
+        alert("O campo nome deve ser preenchido.");
+    }
+});
+
+document.getElementById("continuar").addEventListener("click", function () {
+    let sname = document.getElementById("sname").value;
+    if (!sname) {
+        alert("O campo sobrenome deve ser preenchido.");
+    }
+});
+
+document.getElementById("continuar").addEventListener("click", function () {
+    let email = document.getElementById("email").value;
+    if (!email) {
+        alert("O campo email deve ser preenchido.");
+    }
+});
+
+document.getElementById("continuar").addEventListener("click", function () {
+    let nasc = document.getElementById("nasc").value;
+    if (!nasc) {
+        alert("O campo data de nascimento deve ser preenchido.");
     }
 });
 
