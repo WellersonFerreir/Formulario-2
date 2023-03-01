63841fe95ec81d60365d514c7995f1f7cca6b933
Wellerson Ferreira <117539408+WellersonFerreir@users.noreply.github.com>
2023-03-01 12:57:20 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,6 +4,15 @@ const form = document.getElementById("form");
 const emailRegex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
 const phoneRegex = /^\d{10}$/;
 
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  nameValidate();
+  lastnameValidate();
+  emailValidate();
+  phoneValidate();
+  mainPasswordValidate();
+  comfirmPasswordValidate();
+})
 
 function setError(index) {
   camposDoFormulario[index].style.border = '2px solid #e63636';
@@ -25,11 +34,51 @@ function nameValidate() {
   }
 }
 
+function lastnameValidate() {
+  if (camposDoFormulario[1].value.length < 3) {
+
+    setError(1);
+
+  } else {
+    removeError(1);
+  }
+}
+
 function emailValidate(){
 
-  if (emailRegex.test(camposDoFormulario[2].value)){
-    console.log('vlaido');
-  }else{
-    console.log('nao');
+  if (!emailRegex.test(camposDoFormulario[2].value)){
+    
+    setError(2);
+  } else {
+
+    removeError(2);
+  }
+}
+
+function phoneValidate(){
+
+  if (phoneRegex.test(camposDoFormulario[3].value)){
+    setError(3);
+  } else {
+
+    removeError(3);
+  }
+}
+
+function mainPasswordValidate(){
+  if (camposDoFormulario[4].value.length < 8){
+    setError(4);
+  } else {
+
+    removeError(4);
+    comfirmPasswordValidate();
+  }
+}
+
+function comfirmPasswordValidate(){
+  if (camposDoFormulario[4].value == camposDoFormulario[5].value && camposDoFormulario[5].value.length >= 8){
+    removeError(5);
+  }else {
+    setError(5);
   }
 }
